077e615daa01deaca1cf0709d21a3eb086e801ac
lunarenilson <renilson.c.luna@gmail.com>
2025-07-20 13:21:11 -0300

    Alternate bewtween accounts added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export default function App() {
   const [candidates, setCandidates] = useState([]);
   const [contract, setContract] = useState(null);
+  const [accounts, setAccounts] = useState([]);
+  const [selectedAccount, setSelectedAccount] = useState(0);
+  const [hasVoted, setHasVoted] = useState(false);
 
   useEffect(() => {
     async function loadContract() {
@@ -14,12 +17,21 @@ export default function App() {
         await window.ethereum.request({ method: "eth_requestAccounts" });
 
         const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
-        const signer = await provider.getSigner(3); // conta 0 do Hardhat
+        const signer = await provider.getSigner(selectedAccount); // conta 0 do Hardhat
+        setSelectedAccount(signer.address);
+
+        const accs = await provider.listAccounts();
+        setAccounts(() => accs.map((acc, key) => acc.address));
+
         const voteContract = new ethers.Contract(
           contractAddress,
           abi.abi,
           signer
         );
+
+        const voted = await voteContract.hasVoted(signer.address);
+        setHasVoted(voted)
+        console.log(voted)
 
         setContract(voteContract);
 
@@ -28,7 +40,9 @@ export default function App() {
 
         for (let i = 0; i < numCandidates; i++) {
           const candidateName = await voteContract.candidates(i);
-          const candidateVotes = await voteContract.getCandidateVotes(candidateName)
+          const candidateVotes = await voteContract.getCandidateVotes(
+            candidateName
+          );
           candidates.push({ name: candidateName, votes: candidateVotes });
         }
 
@@ -50,23 +64,47 @@ export default function App() {
 
       alert("Voto enviado com sucesso!");
     } catch (err) {
-      console.error("Erro ao votar:", err.message);
+      alert("Você já votou!!");
+      return;
     }
   };
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Sistema de Votação</h1>
-      {candidates.map((c, i) => (
-        <div key={i} style={{ marginBottom: 15 }}>
-          <strong>
-            [{c.votes}] {c.name}
-          </strong>
-          <button onClick={() => vote(c.name)} style={{ marginLeft: 10 }}>
-            Votar
-          </button>
-        </div>
-      ))}
+    <div className="flex">
+      <div className="vote p-5 m-5">
+        <h1 className="mb-3">Sistema de Votação</h1>
+        {candidates.map((c, i) => (
+          <div key={i} style={{ marginBottom: 15 }}>
+            <strong>
+              [{c.votes}] {c.name}
+            </strong>
+            <button onClick={() => vote(c.name)} style={{ marginLeft: 10 }}>
+              Votar
+            </button>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-col p-5">
+        <label htmlFor="account-select">Selecione a conta:</label>
+        <select
+          id="account-select"
+          value={selectedAccount}
+          onChange={(e) => setSelectedAccount(e.target.value)}
+          className="p-2 rounded border text-white bg-gray-900"
+        >
+          {accounts.map((account) => (
+            <option
+              key={account}
+              value={account}
+              className={`${selectedAccount === account ? "bg-green-600" : ""}`}
+            >
+              {account}''
+            </option>
+          ))}
+        </select>
+        <small className={hasVoted ? 'text-green-500' : 'text-red-700'}>{hasVoted ? 'Já votou!' : "Ainda não votou"}</small>
+      </div>
     </div>
   );
 }
